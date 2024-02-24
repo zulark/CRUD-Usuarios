59cf0b8a16f60ee966334f6509488597a62524d4
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2024-02-24 19:31:31 +0100

    Formulário de cadastro do usuário
    Campo de nome aceita até 100 caracteres e apenas letras
    
    Adicionado alertas de sucesso e erro ao realizar requisições

---
+++ src/pages/formulario/script.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('registrationForm').addEventListener('submit', function (e) {
+        e.preventDefault();
+        const username = document.getElementById('userName').value.trim();
+        const useremail = document.getElementById('userEmail').value.trim();
+
+        if (!username || !useremail) {
+            console.error('Username and email are required.');
+            createErrorAlert('É necessário inserir o nome usuário e o email do usuário.')
+            return; 
+        }
+
+        fetch('http://localhost:3333/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: username,
+                email: useremail
+            }),
+      
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status); 
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            createSuccessAlert('Usuário cadastrado.');
+        })
+        .catch(error => {
+            console.error('Error:', error.message); 
+            createErrorAlert(`Erro ao cadastrar usuário: status ${error.message}`);
+        });
+    });
+});
+
+function createErrorAlert(message) {
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.setAttribute('role', 'alert');
+    errorDiv.textContent = message;
+    errorDiv.style.position = 'fixed'; 
+    errorDiv.style.top = '0'; 
+    errorDiv.style.left = '0'; 
+    errorDiv.style.width = '100%'; 
+    errorDiv.style.zIndex = '9999';
+    document.body.prepend(errorDiv); 
+
+  
+    setTimeout(() => {
+        document.body.removeChild(errorDiv);
+    }, 3000);
+}
+function createSuccessAlert(message) {
+    const successDiv = document.createElement('div');
+    successDiv.className = 'alert alert-success';
+    successDiv.setAttribute('role', 'alert');
+    successDiv.textContent = message;
+    successDiv.style.position = 'fixed';
+    successDiv.style.top = '0'; 
+    successDiv.style.left = '0'; 
+    successDiv.style.width = '100%';  
+    successDiv.style.zIndex = '9999';
+    document.body.prepend(successDiv); 
+
+    setTimeout(() => {
+        document.body.removeChild(successDiv);
+    }, 3000); 
+}

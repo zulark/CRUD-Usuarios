baa699f54ed48375d76fce5f927545cc17647d78
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2024-02-24 20:22:53 +0100

    Limpar campos de email e nome ao enviar e ao recarregar a pagina

--- src/pages/formulario/script.js
+++ src/pages/formulario/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    window.onload = document.getElementById("userEmail").value = ""; document.getElementById("userName").value = "";
+    
     document.getElementById('registrationForm').addEventListener('submit', function (e) {
         e.preventDefault();
         const username = document.getElementById('userName').value.trim();
@@ -7,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!username || !useremail) {
             console.error('Username and email are required.');
             createErrorAlert('É necessário inserir o nome usuário e o email do usuário.')
-            return; 
+            return;
         }
 
         fetch('http://localhost:3333/api/users', {
@@ -19,22 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 name: username,
                 email: useremail
             }),
-      
+
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status); 
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            createSuccessAlert('Usuário cadastrado.');
-        })
-        .catch(error => {
-            console.error('Error:', error.message); 
-            createErrorAlert(`Erro ao cadastrar usuário: status ${error.message}`);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                createSuccessAlert('Usuário cadastrado.');
+                userName.value = '';
+                userEmail.value = '';
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
+                createErrorAlert(`Erro ao cadastrar usuário: status ${error.message}`);
+            });
     });
 });
 
@@ -43,31 +48,31 @@ function createErrorAlert(message) {
     errorDiv.className = 'alert alert-danger';
     errorDiv.setAttribute('role', 'alert');
     errorDiv.textContent = message;
-    errorDiv.style.position = 'fixed'; 
-    errorDiv.style.top = '0'; 
-    errorDiv.style.left = '0'; 
-    errorDiv.style.width = '100%'; 
+    errorDiv.style.position = 'fixed';
+    errorDiv.style.top = '0';
+    errorDiv.style.left = '0';
+    errorDiv.style.width = '100%';
     errorDiv.style.zIndex = '9999';
-    document.body.prepend(errorDiv); 
+    document.body.prepend(errorDiv);
 
-  
     setTimeout(() => {
         document.body.removeChild(errorDiv);
     }, 3000);
 }
+
 function createSuccessAlert(message) {
     const successDiv = document.createElement('div');
     successDiv.className = 'alert alert-success';
     successDiv.setAttribute('role', 'alert');
     successDiv.textContent = message;
     successDiv.style.position = 'fixed';
-    successDiv.style.top = '0'; 
-    successDiv.style.left = '0'; 
-    successDiv.style.width = '100%';  
+    successDiv.style.top = '0';
+    successDiv.style.left = '0';
+    successDiv.style.width = '100%';
     successDiv.style.zIndex = '9999';
-    document.body.prepend(successDiv); 
+    document.body.prepend(successDiv);
 
     setTimeout(() => {
         document.body.removeChild(successDiv);
-    }, 3000); 
+    }, 3000);
 }

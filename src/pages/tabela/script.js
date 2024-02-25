5adb1584724f7901db2860b6958994b8e197e2a2
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2024-02-25 15:45:12 +0100

    Ajustes na tela de tabela
    Adicionado um modal e lógica para edição do usuário
    
    Adicionado comentários no código
    
    Lógica para botão de procura inserida dentro da lógica dos demais botoões para melhor leitura do código

--- src/pages/tabela/script.js
+++ src/pages/tabela/script.js
@@ -31,49 +31,13 @@ function fetchAllUsers() {
         .catch(error => console.error('Error:', error));
 }
 
-fetchAllUsers();
+fetchAllUsers(); 
 
+//Busca todos os usuários e mostra na tabela
 
-function deleteUser(userId) {
-    fetch(`http://localhost:3333/api/users/${userId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            createErrorAlert('Usuário deletado:', data);
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-function editUser(userId) {
-
-    fetch(`http://localhost:3333/api/users/${userId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            createSuccessAlert('Usuário alterado');
-        })
-        .catch(error => console.error('Error:', error));
-}
-
+//Função para buscar os usuários
 function searchUser(userId) {
+
     if (userId == '') {
         fetchAllUsers();
     } else {
@@ -113,28 +77,114 @@ function searchUser(userId) {
                 createErrorAlert('Usuário não encontrado ou erro');
             });
     }
+} 
+//Função para deletar o usuário selecionado
+function deleteUser(userId) {
+    fetch(`http://localhost:3333/api/users/${userId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            createSuccessAlert('Usuário deletado', data);
+        })
+        .catch(error => console.error('Error:', error));
 }
 
+//Função para editar o usuário selecionado
+function editUser(userId) {
 
+    const newUserName = document.getElementById('userName').value;
+    const newUserEmail = document.getElementById('userEmail').value;
+
+    fetch(`http://localhost:3333/api/users/${userId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: newUserName,
+            email: newUserEmail
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+
+        })
+        .then(data => {
+            createSuccessAlert('Usuário alterado', data);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+//Adiciona funcionabilidade para os botões
 document.addEventListener('click', function (e) {
 
     if (e.target.classList.contains('delete-btn')) {
         const userId = e.target.getAttribute('data-id');
         deleteUser(userId);
     }
+
     else if (e.target.classList.contains('edit-btn')) {
         const userId = e.target.getAttribute('data-id');
+        modalHandling(userId);
     }
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('searchBtn').addEventListener('click', function () {
+    else if (e.target.classList.contains('search-btn')){
         const searchId = document.getElementById('searchInput').value;
         searchUser(searchId);
-    });
+    }
+
 });
+ 
+//Logica para o Modal de edição do usuário
+function modalHandling(userId) {
+    var myModal = new bootstrap.Modal(document.getElementById('editUserModal'), {
+        keyboard: false
+    });
 
+    fetch(`http://localhost:3333/api/users/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById("userName").value = data.user.name; 
+        document.getElementById("userEmail").value = data.user.email;
+        myModal.show();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
+    const modalLabel = document.getElementById('editUserModalLabel');
+    modalLabel.innerText = `Editar usuário: ${userId}`
+
+    document.getElementById('saveChanges').addEventListener('click', function () {
+        editUser(userId);
+        myModal.hide();
+    });
+
+}
+
+//Cria alertas de sucesso ou erro para o usuário ao realizar funções no sistema
 function createErrorAlert(message) {
     const errorDiv = document.createElement('div');
     errorDiv.className = 'alert alert-danger';
